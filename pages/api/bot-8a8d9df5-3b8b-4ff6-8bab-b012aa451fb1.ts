5eedad84ee70665da97176e2cdcc53a2b2965f8d
mamatiusupov <dmamatiusupov@demandbase.com>
2023-02-10 19:02:03 +0100

    bububu

--- pages/api/bot-8a8d9df5-3b8b-4ff6-8bab-b012aa451fb1.ts
+++ pages/api/bot-8a8d9df5-3b8b-4ff6-8bab-b012aa451fb1.ts
@@ -6,25 +6,25 @@ import {dicts} from "../../src/text/dicts";
 
 const {message} = require('telegraf/filters');
 
-const bot = new Telegraf(process.env.BOT_TOKEN);
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply(dicts.ru.greet));
-bot.help((ctx) => ctx.reply('Send me a sticker'));
-bot.on(message('sticker'), (ctx: Context) => ctx.reply('👍'));
-bot.hears('hi', (ctx: Context) => ctx.reply('Hey there'));
-bot.on(message("text"), (ctx: Context) => ctx.reply("Hello"));
+  bot.start((ctx) => ctx.reply(dicts.ru.greet));
+  bot.help((ctx) => ctx.reply('Send me a sticker'));
+  bot.on(message('sticker'), (ctx: Context) => ctx.reply('👍'));
+  bot.hears('hi', (ctx: Context) => ctx.reply('Hey there'));
+  bot.on(message("text"), (ctx: Context) => ctx.reply("Hello"));
 
 // Start webhook via launch method (preferred)
-bot.launch({
-  webhook: {
-    domain: process.env.VERCEL_URL,
-    port: 8080,
-    hookPath: "/api/bot-8a8d9df5-3b8b-4ff6-8bab-b012aa451fb1",
-    secretToken: process.env.BOT_SECRET,
-  },
-}).then(res => console.log("Bot launched")).catch(q => console.error("Bot launch failed"));
+  bot.launch({
+    webhook: {
+      domain: process.env.VERCEL_URL,
+      port: 8080,
+      hookPath: "/api/bot-8a8d9df5-3b8b-4ff6-8bab-b012aa451fb1",
+      secretToken: process.env.BOT_SECRET,
+    },
+  }).then(res => console.log("Bot launched")).catch(q => console.error("Bot launch failed"));
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if(req.method === "POST") {
     await bot.handleUpdate(req.body, res);
   } else {
