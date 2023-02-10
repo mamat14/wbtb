96704ea565af387cd7ff745934d943dc94dd48e0
mamatiusupov <dmamatiusupov@demandbase.com>
2023-02-10 19:20:33 +0100

    fixx

--- pages/api/bot-8a8d9df5-3b8b-4ff6-8bab-b012aa451fb1.ts
+++ pages/api/bot-8a8d9df5-3b8b-4ff6-8bab-b012aa451fb1.ts
@@ -6,20 +6,34 @@ import {dicts} from "../../src/text/dicts";
 
 const {message} = require('telegraf/filters');
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+function createBot() {
   const bot = new Telegraf(process.env.BOT_TOKEN);
 
   bot.start((ctx) => ctx.reply(dicts.ru.greet));
   bot.command("schedule", (ctx: Context) => {
     ctx.sendMessage("Привет")
   })
+
+
+
   // bot.help((ctx) => ctx.reply('Send me a sticker'));
   // bot.on(message('sticker'), (ctx: Context) => ctx.reply('👍'));
   // bot.hears('hi', (ctx: Context) => ctx.reply('Hey there'));
   // bot.on(message("text"), (ctx: Context) => ctx.reply("Hello"));
 
+
+  return bot;
+}
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const bot = createBot();
+
   if(req.method === "POST") {
-    await bot.handleUpdate(req.body, res);
+    if(req.headers["X-Telegram-Bot-Api-Secret-Token"] !== process.env.BOT_SECRET) {
+      throw new Error("Not Found")
+    } else {
+      await bot.handleUpdate(req.body, res);
+    }
   } else {
     res.json({"hello": "world"});
   }
