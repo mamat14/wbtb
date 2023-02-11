24213c5fef9438c4a09928ef58d084ac24cccb84
mamatiusupov <dmamatiusupov@demandbase.com>
2023-02-12 00:02:51 +0100

    fixx

--- src/scenes/login.ts
+++ src/scenes/login.ts
@@ -1,5 +1,6 @@
 import {Scenes} from 'telegraf';
 import {MyContext} from "../types";
+import {sendMainMenu} from "../commands/main_menu";
 
 export const LOGIN_DATA_WIZARD_SCENE_ID = 'LOGIN_DATA_WIZARD_SCENE';
 
@@ -10,12 +11,9 @@ export async function startLogin(ctx: MyContext) {
 export const loginDataWizard = new Scenes.WizardScene<MyContext>(
     LOGIN_DATA_WIZARD_SCENE_ID,
     async (ctx: MyContext) => {
-        await ctx.reply(ctx.getDict().enter_hasta_website_login);
-        ctx.session.loginData = {};
-        return ctx.wizard.next();
-    },
-    async (ctx: MyContext) => {
         // validation example
+        ctx.session.loginData = {};
+
         const msg = ctx.message
         if (!("text" in msg) || msg.text.length < 2) {
             await ctx.reply(ctx.getDict().enter_hasta_website_login);
@@ -32,7 +30,7 @@ export const loginDataWizard = new Scenes.WizardScene<MyContext>(
         } else {
             ctx.session.loginData.pwd = msg.text;
             await ctx.reply(ctx.getDict().thank_you_for_login);
-
+            await sendMainMenu(ctx);
             return ctx.scene.leave();
         }
     },
