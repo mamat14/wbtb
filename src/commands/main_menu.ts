26011b7c4b9fea5d0f17be553bd7f1347cce7cec
mamatiusupov <dmamatiusupov@demandbase.com>
2023-02-15 13:17:23 +0100

    fix

--- src/commands/main_menu.ts
+++ src/commands/main_menu.ts
@@ -3,7 +3,8 @@ import {Markup} from "telegraf";
 
 export const sendMainMenu = async (ctx: MyContext) => {
     const lookLeagues = ctx.getDict().look_future_open_leagues;
-    const login = ctx.getDict().login;
-    const MAIN_MENU = Markup.keyboard([[lookLeagues], [login]]).resize();
+    const login = ctx.isLoggedIn() ? [] : [ctx.getDict().login];
+    const actions = [[lookLeagues], login].flatMap(x => x).map(k => [k])
+    const MAIN_MENU = Markup.keyboard(actions).resize();
     await ctx.reply(ctx.getDict().main_menu, MAIN_MENU)
 }
